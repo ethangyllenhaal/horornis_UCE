3f562d6edcba354ba0e3c48ca1dbbe628bfe7449
Ethan Gyllenhaal <egyllenhaal@unm.edu>
2020-08-27 20:26:32 -0600

    Added comments to gene flow comp script

--- R_scripts/horornis_geneflow_comp.R
+++ R_scripts/horornis_geneflow_comp.R
@@ -1,10 +1,11 @@
 library(ggplot2)
 library(tidyverse)
+#import dataset, wrap strings
 setwd('D:/Documents/Projects/Horornis/Files/Demographics')
-dataset <- read_csv('horornis_geneflow_comp.csv')
+dataset <- read_csv('geneflow_est.csv')
 dataset$wrapComp <- str_wrap(dataset$Comparison, width=10)
 
-# log plot
+# log plot, with different shapes and colors for empirical vs estimates
 ggplot(dataset, aes(x=fct_inorder(wrapComp), y=log(Point), ymax=log(Upper), ymin=log(Lower), col=fct_inorder(Method))) +
   geom_pointrange(aes(shape=fct_inorder(Method)), position = position_dodge(1), size=1) +
   scale_shape_manual(values=c(19, 17, 0, 1, 2)) + 
